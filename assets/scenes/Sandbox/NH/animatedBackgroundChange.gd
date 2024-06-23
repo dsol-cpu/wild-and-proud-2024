a61bf5ad6b165dd3f275d4a8b387e69108e4c50d
A Code Fox <heyacfox@gmail.com>
2024-06-23 16:15:20 -0400

    Music, parallax, ambiance, etc

---
+++ assets/scenes/Sandbox/NH/animatedBackgroundChange.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_transition_animation_go()
+	self.animation_looped
+
+func _transition_animation_go():
+	self.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
